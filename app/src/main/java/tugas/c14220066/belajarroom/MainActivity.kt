ebc94ab36d9de221138faf756b7b879d989775ae
Weldante <justinharsono14@gmail.com>
2024-12-03 20:43:27 +0700

    Initial commit

---
+++ app/src/main/java/tugas/c14220066/belajarroom/MainActivity.kt
@@ -0,0 +1,78 @@
+package tugas.c14220066.belajarroom
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import tugas.c14220066.belajarroom.database.daftarBelanja
+import tugas.c14220066.belajarroom.database.daftarBelanjaDB
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var DB : daftarBelanjaDB
+
+    private lateinit var adapterDaftar: adapterDaftar
+
+    private var arDaftar : MutableList<daftarBelanja> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        DB = daftarBelanjaDB.getDatabase(this)
+
+        val _fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        _fabAdd.setOnClickListener {
+            startActivity(Intent(this, TambahDaftar::class.java))
+        }
+
+        adapterDaftar = adapterDaftar(arDaftar)
+        val _rvDafBlanja = findViewById<RecyclerView>(R.id.rvDafBlanja)
+        _rvDafBlanja.layoutManager = LinearLayoutManager(this)
+        _rvDafBlanja.adapter = adapterDaftar
+
+        adapterDaftar.setOnItemClickCallback(
+            object : adapterDaftar.OnItemClickCallback {
+                override fun delData(dtBelanja: daftarBelanja) {
+                    CoroutineScope(Dispatchers.IO).async {
+                        DB.fundaftarBelanjaDAO().delete(dtBelanja)
+                        val daftar = DB.fundaftarBelanjaDAO().selectAll()
+                        withContext(Dispatchers.Main) {
+                            adapterDaftar.isiData(daftar)
+                        }
+                    }
+                }
+
+            }
+        )
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.Main).async {
+            val daftarBelanja = DB.fundaftarBelanjaDAO().selectAll()
+            Log.d("data ROOM", daftarBelanja.toString())
+            adapterDaftar.isiData(daftarBelanja)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/tugas/c14220066/belajarroom/TambahDaftar.kt
@@ -0,0 +1,83 @@
+package tugas.c14220066.belajarroom
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import tugas.c14220066.belajarroom.database.daftarBelanja
+import tugas.c14220066.belajarroom.database.daftarBelanjaDB
+import tugas.c14220066.belajarroom.helper.DateHelper.getCurrentDate
+
+class TambahDaftar : AppCompatActivity() {
+
+    var DB = daftarBelanjaDB.getDatabase(this)
+    var tanggal = getCurrentDate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_daftar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val _etItem = findViewById<EditText>(R.id.etItem)
+        val _etJumlah = findViewById<EditText>(R.id.etJumlah)
+        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+        val _btnUpdate = findViewById<Button>(R.id.btnUpdate)
+
+        _btnTambah.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().insert(
+                    daftarBelanja(
+                        tanggal = tanggal,
+                        item = _etItem.text.toString(),
+                        jumlah = _etJumlah.text.toString()
+                    )
+                )
+            }
+            finish()
+        }
+
+        var iID : Int = 0
+        var iAddEdit : Int = 0
+
+        iID = intent.getIntExtra("id", 0)
+        iAddEdit = intent.getIntExtra("addEdit", 0)
+
+        if (iAddEdit == 0) {
+            _btnTambah.visibility = View.VISIBLE
+            _btnUpdate.visibility = View.GONE
+            _etItem.isEnabled = true
+
+            CoroutineScope(Dispatchers.IO).async {
+                val item = DB.fundaftarBelanjaDAO().getItem(iID)
+                _etItem.setText(item.item)
+                _etJumlah.setText(item.jumlah)
+            }
+        }
+
+        _btnUpdate.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().update(
+                    isi_tanggal = tanggal,
+                    isi_item = _etItem.text.toString(),
+                    isi_jumlah = _etJumlah.text.toString(),
+                    pilihid = iID,
+                    isi_status = 0
+                )
+            }
+            finish()
+        }
+
+    }
+}
